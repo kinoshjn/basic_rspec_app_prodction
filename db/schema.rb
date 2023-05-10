23cb967b9f3c160af8be06557e4382ddba554ff0
Takahiro Kanno <kerochelo@gmail.com>
2023-05-10 17:59:09 +0900

    :+1:add file and db for rails7

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2019_10_07_091857) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_10_085618) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "content"
