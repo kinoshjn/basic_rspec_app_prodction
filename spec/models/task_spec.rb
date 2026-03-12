b09d00abf6f9bcc473851774d0ad695387dd0c65
kinoshjn <kinoshjn@yahoo.co.jp>
2026-03-12 16:24:12 +0900

    テストケースを記載する(describe)

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーションチェック' do
+    it '設定したすべてのバリデーションが機能しているか（titleとstatusがあること、titleが被ってないこと）' do end
+  
+  # 残りのテストパターンを書いていく
+    it 'titleが被らない場合にバリデーションエラーが起きないか' do end
+    it 'titleがない場合にバリデーションが機能してinvalidになるか' do end
+    it 'statusがない場合にバリデーションが機能してinvalidになるか' do end
+    it 'titleが被った場合にuniqueのバリデーションが機能してinvalidになるか' do end
+
+  end
 end
